0592734b856890ed48be86a783271dea5d88a062
Hebelag <denniscatan@gmail.com>
2022-02-14 22:30:02 +0100

    Stage 3 completed!

--- app/src/main/java/com/example/myapplication/flashcards/Main.kt
+++ app/src/main/java/com/example/myapplication/flashcards/Main.kt
@@ -1,4 +1,29 @@
 package com.example.myapplication.flashcards
 
 class Main {
+
+}
+
+fun main() {
+    val cards: MutableList<String> = mutableListOf()
+    val definitions: MutableList<String> = mutableListOf()
+
+    println("Input the number of cards:")
+    val numOfCards: Int = readLine()!!.toInt()
+    repeat(numOfCards) {
+        println("Card #${it + 1}:")
+        cards.add(readLine()!!)
+        println("The definition for card #${it + 1}:")
+        definitions.add(readLine()!!)
+
+    }
+    for( i in 0 until cards.size) {
+        println("Print the definition of \"${cards[i]}\":")
+        val userDefinition: String = readLine()!!
+        if (userDefinition == definitions[i]) {
+            println("Correct!")
+        } else {
+            println("Wrong. The right answer is \"${definitions[i]}\".")
+        }
+    }
 }
