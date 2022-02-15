eade0be7d19f1e657c5c32199e137cfd82044f29
Hebelag <denniscatan@gmail.com>
2022-02-15 11:24:43 +0100

    Stage 4 completed! No OOP, but I guess it starts in the next stage!

--- app/src/main/java/com/example/myapplication/flashcards/Main.kt
+++ app/src/main/java/com/example/myapplication/flashcards/Main.kt
@@ -7,23 +7,51 @@ class Main {
 fun main() {
     val cards: MutableList<String> = mutableListOf()
     val definitions: MutableList<String> = mutableListOf()
+    val flashcards: MutableMap<String, String> = mutableMapOf()
 
     println("Input the number of cards:")
     val numOfCards: Int = readLine()!!.toInt()
     repeat(numOfCards) {
         println("Card #${it + 1}:")
-        cards.add(readLine()!!)
+        var card: String
+        while (true){
+            card = readLine()!!
+            if (flashcards.containsKey(card)) {
+                println("The term \"${card}\" already exists. Try again:")
+            } else {
+                break
+            }
+        }
+
         println("The definition for card #${it + 1}:")
-        definitions.add(readLine()!!)
+        var definition: String
+        while (true) {
+            definition = readLine()!!
+            if (flashcards.containsValue(definition)) {
+                println("The definition \"${definition}\" already exists. Try again:")
+            } else {
+                break
+            }
+        }
+        flashcards[card] = definition
+
 
     }
-    for( i in 0 until cards.size) {
-        println("Print the definition of \"${cards[i]}\":")
+    for( (card, definition) in flashcards) {
+        println("Print the definition of \"${card}\":")
         val userDefinition: String = readLine()!!
-        if (userDefinition == definitions[i]) {
-            println("Correct!")
-        } else {
-            println("Wrong. The right answer is \"${definitions[i]}\".")
+        when {
+            userDefinition == definition -> {
+                println("Correct!")
+            }
+            flashcards.containsValue(userDefinition) -> {
+                val correctDefinition = flashcards.filterValues { it == userDefinition }
+                println("Wrong. The right answer is \"${definition}\", but your definition is correct for \"${correctDefinition.keys.joinToString(", ")}\"")
+            }
+            else -> {
+                println("Wrong. The right answer is \"${definition}\".")
+            }
         }
     }
+
 }
